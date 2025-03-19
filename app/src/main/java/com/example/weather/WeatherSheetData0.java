a99e91da638b0de5859f70a9ef41e5274e34dd61
Arslan-raf <61138541+Arslan-raf@users.noreply.github.com>
2025-03-19 21:50:32 +0500

    исправшил ошибку с рандомом

--- app/src/main/java/com/example/weather/WeatherSheetData0.java
+++ app/src/main/java/com/example/weather/WeatherSheetData0.java
@@ -1,16 +1,18 @@
 package com.example.weather;
-import java.util.random.RandomGenerator;
+//import java.util.random.RandomGenerator;
+import java.util.Random;
 
 public class WeatherSheetData0 {
+    private final Random random = new Random();
     /** Возвращают новые показатели **/
     public float getTemperature() {
-        return RandomGenerator.getDefault().nextFloat(0, 30);
+        return random.nextFloat() * 30;
     }
     public float getHumidity(){
-        return RandomGenerator.getDefault().nextFloat(10, 100);
+        return 10 + random.nextFloat() * 90;
     }
     public float getPressure(){
-        return RandomGenerator.getDefault().nextFloat(740, 760);
+        return 740 + random.nextFloat() * 20;
     }
 
     /** Вызывается при каждом обновлении показателей датчиков (как он вызывается - не важно) **/
